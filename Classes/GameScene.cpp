fd736c7c4590d28a9efb71928e4ce65c8de62544
aokht <aokht.zxc.v@gmail.com>
2015-07-01 14:17:45 -0700

    Created GameScene

---
+++ Classes/GameScene.cpp
@@ -0,0 +1,18 @@
+//
+//  GameScene.cpp
+//  Tetrominos
+//
+//  Created by Akihito OKUHATA on 2015/07/01.
+//
+//
+
+#include "GameScene.h"
+
+bool GameScene::init()
+{
+    if (! Node::init()) {
+        return false;
+    }
+
+    return true;
+}
